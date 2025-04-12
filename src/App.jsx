890a3f25181349513d16c69e999cd70df08bf1cf
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-12 15:30:11 +0300

    Addsorting functinality by name

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,33 @@ import Form from "./components/Form";
 
 export default function App() {
   const [students, setStudents] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   const handleAddStudent = (newStudent) => {
     setStudents((prev) => [...prev, newStudent]);
   };
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+  //filter based on search
+
+  const filteredStudents = students.filter((student) =>
+    student.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <div className="container">
       <Form onAddStudent={handleAddStudent} />
-      <StudentTable data={students} />
+      <div style={{ flex: 1 }}>
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          style={{ marginBottom: "1rem", padding: "0.5rem", width: "100%" }}
+        />
+      </div>
+      <StudentTable data={filteredStudents} />
     </div>
   );
 }
